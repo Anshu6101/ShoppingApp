fdad528d151c7191c61b88dd5b96a35c09a31165
Anshu6101 <anshumandhane2125@gmail.com>
2025-02-03 23:22:26 +0530

    Initial commit

---
+++ script.js
@@ -0,0 +1,39 @@
+const imgs = document.querySelectorAll(".header-slider ul img");
+const prev_btn = document.querySelector(".control_prev");
+const next_btn = document.querySelector(".control_next");
+
+let n = 0;
+
+function changeSlide() {
+  for (let i = 0; i < imgs.length; i++) {
+    imgs[i].style.display = "none"; // it selects all the images available there.
+  }
+  imgs[n].style.display = "block"; //This will be taking the 1st image, when n = 0. if n = 1 it will take 2nd img and if n = 0 it will take 3rd img.
+}
+changeSlide();
+
+prev_btn.addEventListener("click", (e) => {
+  if (n > 0) {
+    n--;
+  } else {
+    n = imgs.length - 1;
+  }
+  changeSlide();
+});
+
+next_btn.addEventListener("click", (e) => {
+  if (n < imgs.length - 1) {
+    n++;
+  } else {
+    n = 0;
+  }
+  changeSlide();
+});
+
+const scrollContainer = document.querySelectorAll(".products");
+for (const item of scrollContainer) {
+  item.addEventListener("wheel", (evt) => {
+    evt.preventDefault();
+    item.scrollLeft += evt.deltaY;
+  });
+}
